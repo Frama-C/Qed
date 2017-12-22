3fe9cf1b1f61e134a4a89fbbe9364660ddb338e6
Loïc Correnson <loic.correnson@cea.fr>
2017-12-22 16:14:22 +0100

    Zarith dependencies

--- frama-c.sh
+++ frama-c.sh
@@ -9,24 +9,30 @@ then
     BRANCH="master"
 fi
 
-echo "Archiving 'frama-c:$BRANCH'..."
+echo "Branching"
+
+git checkout -B frama-c/$BRANCH
+
+echo "Archiving 'frama-c:$BRANCH'"
 
 git archive \
     --remote git@git.frama-c.com:frama-c/frama-c.git \
     --format=tgz -o .frama-c.tgz $BRANCH
 
-echo "Extracting the archive..."
+echo "Extracting the archive"
 
 rm -fr .frama-c.tmp
 mkdir .frama-c.tmp
 cd .frama-c.tmp && tar zxf ../.frama-c.tgz && cd ..
 
-echo "Importing OCaml files..."
+echo "Importing OCaml files"
 
+rm -fr src
+mkdir -p src
 rm -f .frama-c.tmp/src/plugins/qed/QedGui.ml
 cp .frama-c.tmp/src/plugins/qed/*.ml* src/
 
-echo "Cleaning..."
+echo "Cleaning"
 
 rm -fr .frama-c.tmp .frama-c.tgz
 make lint
